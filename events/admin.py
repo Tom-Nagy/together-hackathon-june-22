09dd29b0391ac5f09ad3b3b767908e1db7204283
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-06-23 18:41:44 +0200

    Events app (#113)
    * Start event app
    
    * Add event model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import event
+
 # Register your models here.
+
+class AdminEvents(admin.ModelAdmin):
+    list_display = (
+        'event_name',
+        'date',
+        'location',
+    )
+    ordering= ('-date',)
+
+
+admin.site.register(event, AdminEvents)
+
--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class event(models.Model):
+    class Meta:
+        db_table = 'events'
+
+    event_id= models.AutoField(primary_key=True)
+    event_name= models.CharField(max_length=50, blank=False, null=False)
+    date= models.DateField(blank=False, null=False)
+    location= models.CharField(max_length=50, blank=False, null=False)
+    description= models.TextField(max_length=400, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.event_name
+
