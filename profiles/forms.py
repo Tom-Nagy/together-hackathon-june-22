7a8345f5c63499047088fc0e859daac198beb8f8
Tom-Nagy <tomnagy@hotmail.fr>
2022-06-24 12:30:16 +0000

    Add custom widget and styles for image input select

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
 '''Create and configure the profile form'''
 
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import UserProfile
 
 
@@ -9,17 +10,27 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         ''' Form meta properties '''
         model = UserProfile
-        fields = ('username', 'email', 'about',)
+        fields = ('image', 'about',)
+
+    # Replace the image field the the custom widget
+    image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
         placeholders = {
-            'username': 'username',
-            'email': 'email address',
             'about': 'Tell us who you are...',
         }
 
         for field in self.fields:
-            placeholder = f'{placeholders[field]}'
-        self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'image':
+                placeholder = f'{placeholders[field]}'
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
+
+        # Add custom class to the image field
+        for field_name, field in self.fields.items():
+            if field_name == 'image':
+                field.widget.attrs['class'] = 'custom-image-input'
--- profiles/views.py
+++ profiles/views.py
@@ -5,14 +5,33 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 @login_required
 def profile(request):
-    ''' Display the user profile information '''
+    ''' Display and edit the user profile information '''
+
     user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        # update user profile information
+        profile_form = UserProfileForm(request.POST, instance=user_profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'something went wrong \
+                                     Please make sure inforation are valid \
+                                     or contact us for assistance.')
+            return redirect(reverse('profile'))
+    else:
+        profile_form = UserProfileForm(instance=user_profile)
+
     template = 'profiles/profile.html'
     context = {
         'user_profile': user_profile,
+        'profile_form': profile_form,
     }
+
     return render(request, template, context)
