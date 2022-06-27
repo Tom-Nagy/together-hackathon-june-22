2fdeb8a6c98110e3f495a15811c1c3fc07ba1b75
Tom-Nagy <tomnagy@hotmail.fr>
2022-06-27 15:18:08 +0000

    Add admin for favorites and meta to favorites models for verbose name

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,12 @@ from chatrooms.models import Chatroom, Comment
 
 
 class FavoritesChatrooms(models.Model):
-    ''' Model that define the favorites database design '''
+    ''' Model that define the favorites chatroom database design '''
+
+    class Meta:
+        '''Set verbose name'''
+        verbose_name_plural = 'Favorites Chatrooms'
+
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='favorites_chatrooms')
     chatroom = models.ForeignKey(Chatroom, on_delete=models.SET_NULL,
@@ -17,7 +22,12 @@ class FavoritesChatrooms(models.Model):
 
 
 class FavoritesComments(models.Model):
-    ''' Model that define the favorites database design '''
+    ''' Model that define the favorites comment database design '''
+
+    class Meta:
+        '''Set verbose name'''
+        verbose_name_plural = 'Favorites Comments'
+
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='favorites_comments')
     comment = models.ForeignKey(Comment, on_delete=models.SET_NULL,
