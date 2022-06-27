a4c660bb017c4c8fb562764f1542f73f4ee8c490
Tom-Nagy <tomnagy@hotmail.fr>
2022-06-27 10:08:27 +0000

    Create a view to separate make profile public or private functionality from the profile view

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('delete_profile', views.delete_profile, name='delete_profile'),
+    path('delete_profile/', views.delete_profile, name='delete_profile'),
+    path('edit_make_profile_public_status/',
+         views.edit_make_profile_public_status,
+         name='edit_make_profile_public_status'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -11,43 +11,33 @@ from .forms import UserProfileForm
 
 @login_required
 def profile(request):
-    ''' Display and edit the user profile information '''
+    ''' Display and Edit the user profile information '''
 
     user_profile = get_object_or_404(UserProfile, user=request.user)
+    user = User.objects.get(username=request.user.username)
 
     if request.method == 'POST':
+        # update user profile information
+        profile_form = UserProfileForm(request.POST, request.FILES,
+                                    instance=user_profile)
+        if profile_form.is_valid():
+            profile_form.save()
 
-        if request.POST['make_profile_public']:
-            make_profile_public = request.POST['make_profile_public']
-            user_profile.make_public = make_profile_public
-            user_profile.save()
-            if make_profile_public == 'True':
-                messages.success(request, 'Your profile is now public')
-            else:
-                messages.success(request, 'Your profile is not public anymore')
+            # Update User info
+            user = User.objects.get(username=request.user.username)
+            username = request.POST['username']
+            email = request.POST['email']
+            user.username = username
+            user.email = email
+            user.save()
+
+            messages.success(request, 'Profile updated successfully')
             return redirect('profile')
         else:
-            # update user profile information
-            profile_form = UserProfileForm(request.POST, request.FILES,
-                                        instance=user_profile)
-            if profile_form.is_valid():
-                profile_form.save()
-
-                # Update User info
-                user = User.objects.get(username=request.user.username)
-                username = request.POST['username']
-                email = request.POST['email']
-                user.username = username
-                user.email = email
-                user.save()
-
-                messages.success(request, 'Profile updated successfully')
-                return redirect('profile')
-            else:
-                messages.error(request, 'something went wrong \
-                                        Please make sure inforation are valid \
-                                        or contact us for assistance.')
-                return redirect(reverse('profile'))
+            messages.error(request, 'something went wrong \
+                                    Please make sure inforation are valid \
+                                    or contact us for assistance.')
+            return redirect(reverse('profile'))
 
     profile_form = UserProfileForm(instance=user_profile)
     template = 'profiles/profile.html'
@@ -60,14 +50,30 @@ def profile(request):
 
 
 @login_required
+def edit_make_profile_public_status(request):
+    ''' Edit/switch the status of make profile public '''
+
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        make_profile_public = request.POST['make_profile_public']
+        user_profile.make_public = make_profile_public
+        user_profile.save()
+        if make_profile_public == 'True':
+            messages.success(request, 'Your profile is now public')
+        else:
+            messages.success(request, 'Your profile is now private')
+
+        return redirect('profile')
+
+
+@login_required
 def delete_profile(request):
     ''' Delete the user profile and the user '''
 
-    # user_profile = get_object_or_404(UserProfile, user=request.user)
     user = User.objects.get(username=request.user.username)
 
     if request.method == 'POST':
-        # user_profile.delete()
         user.delete()
 
         messages.success(request, 'Your profile was deleted successfuly')
